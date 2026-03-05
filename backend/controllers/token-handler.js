780f459615f2aff93caa098620a71e794276824c
Mr. Kartik Saini <mr.kartiksaini23@gmail.com>
2026-03-05 22:39:02 +0530

    update refresh and access token logic

--- backend/controllers/token-handler.js
+++ backend/controllers/token-handler.js
@@ -1,19 +1,50 @@
 const jwt = require("jsonwebtoken");
 
-function generateAccessToken(userId) {
-  return jwt.sign(
-    { userId },
-    process.env.ACCESS_TOKEN_SECRET,
-    { expiresIn: "30s" }
-  );
+const ACCESS_TOKEN_SECRET =
+  process?.env?.ACCESS_TOKEN_SECRET || "ACCESS_TOKEN_SECRET";
+const ACCESS_TOKEN_EXPIRE_IN = process?.env?.ACCESS_TOKEN_EXPIRE_IN || "30s";
+
+const REFRESH_TOKEN_SECRET =
+  process?.env?.REFRESH_TOKEN_SECRET || "REFRESH_TOKEN_SECRET ";
+const REFRESH_TOKEN_EXPIRE_IN = process?.env?.REFRESH_TOKEN_EXPIRE_IN || "7d";
+
+function generateAccessToken({ _id, name, email }) {
+  return jwt.sign({ user: { _id, name, email } }, ACCESS_TOKEN_SECRET, {
+    expiresIn: ACCESS_TOKEN_EXPIRE_IN,
+    algorithm: "HS256",
+  });
 }
 
 function generateRefreshToken(userId) {
-  return jwt.sign(
-    { userId },
-    process.env.REFRESH_TOKEN_SECRET,
-    { expiresIn: "7d" }
-  );
+  return jwt.sign({ userId }, REFRESH_TOKEN_SECRET, {
+    expiresIn: REFRESH_TOKEN_EXPIRE_IN,
+    algorithm: "HS256",
+  });
 }
 
-module.exports = { generateAccessToken, generateRefreshToken };
+function verifyAccessToken(token) {
+  return jwt.verify(token, ACCESS_TOKEN_SECRET, {}, function (err, decoded) {
+    if (err) {
+      throw new Error(err?.message);
+    } else {
+      return decoded;
+    }
+  });
+}
+
+function verifyRefreshToken(token) {
+  return jwt.verify(token, REFRESH_TOKEN_SECRET, {}, function (err, decoded) {
+    if (err) {
+      throw new Error(err?.message);
+    } else {
+      return decoded;
+    }
+  });
+}
+
+module.exports = {
+  generateAccessToken,
+  generateRefreshToken,
+  verifyAccessToken,
+  verifyRefreshToken,
+};
--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -4,30 +4,33 @@ const bcrypt = require("bcrypt");
 const User = require("../models/User.model");
 const { Api_Erorr_Response, Api_Response } = require("./response-handler");
 
-const JWT_SECRET = process?.env?.JWT_SECRET;
-const JWT_TOKEN_EXPIRE_IN = process?.env?.JWT_TOKEN_EXPIRE_IN;
-
-const { generateRefreshToken } = require("./token-handler");
-
-const generateToken = ({ _id, name, email }) => {
-  return jwt.sign({ user: { _id, name, email } }, JWT_SECRET, {
-    expiresIn: "30s",
-  });
-};
+const {
+  generateRefreshToken,
+  generateAccessToken,
+  verifyRefreshToken,
+} = require("./token-handler");
 
 // REGISTER USER ---------
 
-const registerUser = async (req, res) => {
-  const { name, email, password } = req.body;
+const COOKIES_REFRESH_TOKEN_OPTIONS = {
+  maxAge: 1 * 1000 * 60 * 60 * 24 * 7, 
+  httpOnly: true,
+  secure: true,
+  path: "/auth/refresh",
+};
 
-  // Check if user already exists
-  const userExists = await User.findOne({ email });
-  if (userExists) {
-    return res
-      .status(400)
-      .json(Api_Erorr_Response({ message: "User already exists" }));
-  }
+const registerUser = async (req, res) => {
   try {
+    const { name, email, password } = req.body;
+
+    // Check if user already exists
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res
+        .status(400)
+        .json(Api_Erorr_Response({ message: "User already exists" }));
+    }
+
     const user = await User.create({
       name,
       email,
@@ -43,78 +46,106 @@ const registerUser = async (req, res) => {
       user.refreshToken = refreshToken;
       await user.save();
 
-      res.status(201).json(
+      res.cookie("refreshToken", refreshToken, COOKIES_REFRESH_TOKEN_OPTIONS);
+
+      return res.status(201).json(
         Api_Response({
           user: data,
-          token: generateToken(data),
-        })
+          token: generateAccessToken(data),
+        }),
       );
     } else {
-      res
+      return res
         .status(400)
         .json(Api_Erorr_Response({ message: "Invalid user data" }));
     }
   } catch (error) {
-    console.error(error.message);
-    res.status(500).json({ message: "Server error during registration" });
+    return res
+      .status(500)
+      .json({ message: "Server error during registration" });
   }
 };
 
 // LOGIN USER ---------
 
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+  try {
+    const { email, password } = req.body;
 
-  const user = await User.findOne(
-    { email },
-    { _id: 1, name: 1, email: 1, password: 1 }
-  );
-
-  if (user === null) {
-    res.status(404).json(Api_Erorr_Response({ message: "User not found" }));
-  }
-
-  const isPasswordMatched = await bcrypt.compare(password, user.password);
-
-  if (user && isPasswordMatched) {
-    const { _id, name, email } = user;
-    const data = { _id, name, email };
-
-    const refreshToken = generateRefreshToken(user._id);
-
-    user.refreshToken = refreshToken;
-    await user.save();
-
-    res.json(
-      Api_Response({
-        user: data,
-        token: generateToken(data),
-        refreshToken,
-      })
+    const user = await User.findOne(
+      { email },
+      { _id: 1, name: 1, email: 1, password: 1 },
     );
-  } else {
-    res.status(404).json(Api_Erorr_Response({ message: "Incorrect Password" }));
+    if (user === null) {
+      return res
+        .status(404)
+        .json(Api_Erorr_Response({ message: "User not found" }));
+    }
+
+    const isPasswordMatched = await bcrypt.compare(password, user.password);
+
+    if (user && isPasswordMatched) {
+      const { _id, name, email } = user;
+      const data = { _id, name, email };
+
+      const refreshToken = generateRefreshToken(user._id);
+
+      user.refreshToken = refreshToken;
+      await user.save();
+
+      res.cookie("refreshToken", refreshToken, COOKIES_REFRESH_TOKEN_OPTIONS);
+
+      return res.json(
+        Api_Response({
+          user: data,
+          token: generateAccessToken(data),
+          refreshToken,
+        }),
+      );
+    } else {
+      return res
+        .status(404)
+        .json(Api_Erorr_Response({ message: "Incorrect Password" }));
+    }
+  } catch (error) {
+    return res.status(400).json({ message: error?.message });
   }
 };
 
 const refresh = async (req, res) => {
-  const { refreshToken } = req.body;
+  try {
+    const { refreshToken } = req.body;
 
-  if (!refreshToken) return res.sendStatus(401);
+    if (!refreshToken) throw new Error("refreshToken not found in payload");
 
-  const user = await User.findOne(
-    { refreshToken },
-    { _id: 1, name: 1, email: 1, password: 1 }
-  );
-  if (!user) return res.sendStatus(403);
+    const isJWTRefreshTokenValid = verifyRefreshToken(refreshToken);
 
-  jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, decoded) => {
-    if (err) return res.sendStatus(403);
+    const user = await User.findOne(
+      { _id: isJWTRefreshTokenValid.userId },
+      { _id: 1, name: 1, email: 1, refreshToken: 1 },
+    );
+    if (!user) throw new Error("User not found");
+
+    if (refreshToken !== user.refreshToken) {
+      throw new Error("Refresh Token is not vaild.");
+    }
 
     const { _id, name, email } = user;
-    const newAccessToken = generateToken({ _id, name, email });
-    return res.status(201).json(Api_Response({ accessToken: newAccessToken }));
-  });
+    const newRefreshToken = generateRefreshToken(user._id);
+    const newAccessToken = generateAccessToken({ _id, name, email });
+    user.refreshToken = newRefreshToken;
+    await user.save();
+
+    res.cookie("refreshToken", refreshToken, COOKIES_REFRESH_TOKEN_OPTIONS);
+    return res.status(201).json(
+      Api_Response({
+        accessToken: newAccessToken,
+        refreshToken: newRefreshToken,
+      }),
+    );
+  } catch (error) {
+    res.status(403).json(Api_Erorr_Response({ message: error?.message }));
+  }
 };
 
 const logout = async (req, res) => {
